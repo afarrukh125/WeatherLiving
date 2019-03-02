aac4ac793262dfa41a54607d5f75f276569f0105
ec17119 <a.farrukh@se17.qmul.ac.uk>
2019-03-02 01:01:20 +0000

    Options from the dropdown menu are now selectable.

--- src/components/LocationDropdown.jsx
+++ src/components/LocationDropdown.jsx
@@ -3,22 +3,35 @@ import DropdownButton from "react-bootstrap/Dropdown";
 import Dropdown from "react-bootstrap/Dropdown";
 
 class LocationDropdown extends Component {
-  state = {};
+  state = {
+    selectedIdx: 0,
+    options: ["Select Desired Weather", "Similar", "Warmer", "Colder"]
+  };
   render() {
     return (
       <Dropdown alignRight>
         <Dropdown.Toggle variant="secondary" id="dropdown-basic">
-          Select Desired Weather
+          {this.state.options[this.state.selectedIdx]}
         </Dropdown.Toggle>
 
         <Dropdown.Menu>
-          <Dropdown.Item>Similar</Dropdown.Item>
-          <Dropdown.Item>Colder</Dropdown.Item>
-          <Dropdown.Item>Warmer</Dropdown.Item>
+          <Dropdown.Item eventKey="1" onSelect={this.handleOption}>
+            {this.state.options[1]}
+          </Dropdown.Item>
+          <Dropdown.Item eventKey="2" onSelect={this.handleOption}>
+            {this.state.options[2]}
+          </Dropdown.Item>
+          <Dropdown.Item eventKey="3" onSelect={this.handleOption}>
+            {this.state.options[3]}
+          </Dropdown.Item>
         </Dropdown.Menu>
       </Dropdown>
     );
   }
+
+  handleOption = idx => {
+    this.setState({ selectedIdx: idx });
+  };
 }
 
 export default LocationDropdown;
